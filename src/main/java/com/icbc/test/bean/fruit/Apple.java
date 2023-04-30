0e45baa9024750414defccd7c3520d83c3acf1d1
wade0127 <1254408737@qq.com>
2023-05-01 00:26:15 +0800

    完美解决问题

---
+++ src/main/java/com/icbc/test/bean/fruit/Apple.java
@@ -0,0 +1,16 @@
+package com.icbc.test.bean.fruit;
+
+/**
+ * @BelongsProject: test_icbc
+ * @BelongsPackage: com.icbc.test.bean
+ * @Author: wade
+ * @CreateTime: 2023-04-30  09:36
+ * @Description: Æ»¹û
+ * @Version: 1.0
+ */
+public class Apple extends Fruit {
+    public Apple() {
+        //Æ»¹û£¬8Ôª/½ï
+        super(FruitType.Apple,"apple", 8);
+    }
+}
